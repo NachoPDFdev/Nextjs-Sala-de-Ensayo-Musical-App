d46394b5666d51389df89bca8b14b24981b2ddb4
NachoPDFdev <sourcenyt@gmail.com>
2024-08-29 23:45:05 -0400

    v5.1
    v5.1

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import RehearsalRoomBooking from './RehearsalRoomBooking'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold mb-8">NextJS Music</h1>
+      <h1 className="text-4xl font-bold mb-8">Hola</h1>
       <RehearsalRoomBooking />
     </main>
   )
